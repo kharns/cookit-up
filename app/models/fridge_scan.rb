406140d4221dc7e4e479adde309281b19fc0b2a5
kristen harnois <kristen.harnois@gmail.com>
2024-11-25 15:54:44 +0100

    created associations

--- app/models/fridge_scan.rb
+++ app/models/fridge_scan.rb
@@ -1,4 +1,5 @@
 class FridgeScan < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
+  has_many :recipes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :fridge_scans
+  has_many :recipes, through: :fridge_scans
 end
