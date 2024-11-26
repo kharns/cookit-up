92fbdfbe9542a332e03d6d6267c2d41de22cc153
kristen harnois <kristen.harnois@gmail.com>
2024-11-26 14:06:22 +0100

    started fridge_scan create action

--- app/controllers/fridge_scans_controller.rb
+++ app/controllers/fridge_scans_controller.rb
@@ -1,7 +1,25 @@
 class FridgeScansController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:new]
+  skip_before_action :authenticate_user!, only: %i[new create]
 
   def new
     @fridge_scan = FridgeScan.new
+  end
+
+  def create
+    @fridge_scan = FridgeScan.new(scan_params)
+    # User_id => optional
+    @fridge_scan.user = current_user unless current_user.nil?
+    @fridge_scan.save
+    raise
+    # Ingredient_list => OpenAI
+    #  CHATGPT PROMPT TEMPLATE FOR IMAGE (VISION)
+
+
+  end
+
+  private
+
+  def scan_params
+    params.require(:fridge_scan).permit(:photo)
   end
 end
