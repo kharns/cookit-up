b8b1ef4443c983ab03c0f2d0e8fbfd04456c0809
Nina Hubault <nhubault@protonmail.com>
2024-11-27 17:52:26 +0100

    favoris

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,27 @@
 class RecipesController < ApplicationController
+  def index
+    fridge_scan = FridgeScan.find(params[:fridge_scan_id])
+    @recipes = fridge_scan.recipes
+  end
+
+  def add_favorite
+    @recipe = Recipe.find(params[:id])
+    @recipe.favourite = true
+    if @recipe.save!
+      redirect_to fridge_scan_recipes_path(FridgeScan.find(params[:fridge_scan_id]))
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def remove_favorite
+    @recipe = Recipe.find(params[:id])
+    @recipe.favourite = false
+    if @recipe.save!
+      redirect_to fridge_scan_recipes_path(FridgeScan.find(params[:fridge_scan_id]))
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
 end
