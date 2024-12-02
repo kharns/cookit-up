8c253cb689f06c47a2efd8508e803f772aa1fd92
kristen harnois <kristen.harnois@gmail.com>
2024-12-02 13:21:47 +0100

    added waiting page when looking for ingredients

---
+++ app/javascript/controllers/waiting_page_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="waiting-page"
+export default class extends Controller {
+  static targets = ["newScanDom", "form"]
+  connect() {
+    console.log(this.newScanDomTarget.innerHTML)
+  }
+
+  wait(event) {
+    event.preventDefault()
+
+    if (this.formTarget.checkValidity() === true) {
+      this.newScanDomTarget.innerHTML = `<div class="waiting-gif">
+                                        <h3>Looking for your ingredients</h3>
+                                        <img id="gif" class="nlSABoG9CSaJpsufv8WW9 _3vYn8QjoEvrXxHyqdn9ddZ _2XBDTIVigBJDybhZvL-hU3" src="https://media2.giphy.com/media/3o6gEd7LbHXWWiLbuE/200w.webp?cid=dda24d50986bef2kcb6251do3b8rr76vlt2cfhhgp3kxpteh&amp;ep=v1_internal_gif_by_id&amp;rid=200w.webp&amp;ct=g" srcset="https://media2.giphy.com/media/3o6gEd7LbHXWWiLbuE/200w.webp?cid=dda24d50986bef2kcb6251do3b8rr76vlt2cfhhgp3kxpteh&amp;ep=v1_internal_gif_by_id&amp;rid=200w.webp&amp;ct=g 200w,https://media2.giphy.com/media/3o6gEd7LbHXWWiLbuE/giphy.webp?cid=dda24d50986bef2kcb6251do3b8rr76vlt2cfhhgp3kxpteh&amp;ep=v1_internal_gif_by_id&amp;rid=giphy.webp&amp;ct=g 480w," alt="Fridge scanning gif"></img>
+                                        </div>`
+    }
+  }
+}
