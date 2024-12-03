924747e1f487d8344708b0490a86d8007511bc32
kristen harnois <kristen.harnois@gmail.com>
2024-12-03 11:36:54 +0100

    changed css for waiting pages

--- app/javascript/controllers/waiting_page_controller.js
+++ app/javascript/controllers/waiting_page_controller.js
@@ -4,14 +4,13 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["waitingDom", "form", "dom"]
   connect() {
-    console.log("connecte coco")
   }
 
   wait(event) {
-    if (this.formTarget.checkValidity() === true) {
-
-      if (this.domTarget.id === "new_fridge_scan") {
-        this.formTarget.classList.add("d-none")
+    const form = this.formTarget
+    if (form.checkValidity() === true) {
+      if (form.id === "new_fridge_scan") {
+        form.classList.add("d-none")
         this.waitingDomTarget.innerHTML = `<div class="waiting-scan-gif">
         <h3>Looking for your ingredients</h3>
         <img id="gif" class="nlSABoG9CSaJpsufv8WW9 _3vYn8QjoEvrXxHyqdn9ddZ _2XBDTIVigBJDybhZvL-hU3" src="https://media2.giphy.com/media/3o6gEd7LbHXWWiLbuE/200w.webp?cid=dda24d50986bef2kcb6251do3b8rr76vlt2cfhhgp3kxpteh&amp;ep=v1_internal_gif_by_id&amp;rid=200w.webp&amp;ct=g" srcset="https://media2.giphy.com/media/3o6gEd7LbHXWWiLbuE/200w.webp?cid=dda24d50986bef2kcb6251do3b8rr76vlt2cfhhgp3kxpteh&amp;ep=v1_internal_gif_by_id&amp;rid=200w.webp&amp;ct=g 200w,https://media2.giphy.com/media/3o6gEd7LbHXWWiLbuE/giphy.webp?cid=dda24d50986bef2kcb6251do3b8rr76vlt2cfhhgp3kxpteh&amp;ep=v1_internal_gif_by_id&amp;rid=giphy.webp&amp;ct=g 480w," alt="Fridge scanning gif"></img>
