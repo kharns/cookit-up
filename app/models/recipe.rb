18332665e67d32d6a739967451a9c0c60809c477
Kristen Harnois <145620308+kharns@users.noreply.github.com>
2024-12-05 12:42:28 +0100

    Revert "Pgsearch"

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,18 +1,10 @@
 class Recipe < ApplicationRecord
-  include PgSearch::Model
-
   belongs_to :fridge_scan
   has_one_attached :photo
 
   validates :title, :guest, :ingredient_list, :content, :difficulty, :cooking_time, presence: true
 
   after_create_commit :generate_photo
-
-  pg_search_scope :search_by_all,
-  against: [ :title, :ingredient_list, :content ],
-  using: {
-    tsearch: { prefix: true }
-  }
 
   private
 
